e328976f290c2374af1760e815391c12b29e2091
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-10-18 20:55:18 -0700

    changed database name

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
-CREATE DATABASE employeeTracker_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employeeTracker_db;
+USE employee_db;
 
 CREATE TABLE department(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
